790377140cfc54cead6ff6f6c3599e01f59c7479
ardak <ardak.tileu@mediamonks.com>
2022-09-01 15:46:15 +0600

    Implement virtual list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { getItem, FeedItem } from "./utils/data.utils";
 import { db } from "./utils/db.utils";
-import { LazyListComponent } from "./lazy-list/lazy-list";
+import { VirtualListComponent } from "./virtual-list/virtual-list";
 
 export const templateFn = (item: FeedItem) => {
   return `<section class="feed__item">
@@ -12,12 +12,22 @@ export const templateFn = (item: FeedItem) => {
   </section>`
 }
 
+const updateItemFn = (element: HTMLElement, { url, description, name }: FeedItem) => {
+  element.querySelector<HTMLImageElement>('.feed__item__img').src = url;
+  element.querySelector('h2').innerHTML = name;
+  element.querySelector('p').innerHTML = description;
+
+  return element;
+}
+
 const DB_SIZE = 1000;
 const root: HTMLDivElement = document.getElementById('app') as HTMLDivElement;
 const DB = db(DB_SIZE, DB_SIZE, getItem);
-const feed = new LazyListComponent<FeedItem>(root, {
+const feed = new VirtualListComponent<FeedItem>(root, {
   templateFn,
   load: (start, limit) => DB.load(start, limit).then((cursor) => cursor.chunk),
-  pageSize: 10
+  pageSize: 10,
+  itemMargin: 16,
+  updateItemFn
 })
 feed.render();
